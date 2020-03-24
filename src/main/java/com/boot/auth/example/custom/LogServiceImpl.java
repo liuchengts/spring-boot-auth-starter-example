3f2547217eb0112fd979f3ceee8d9df8c0c953a4
liucheng <790809568@qq.com>
2020-03-24 15:12:27 +0800

    日志级别

--- src/main/java/com/boot/auth/example/custom/LogServiceImpl.java
+++ src/main/java/com/boot/auth/example/custom/LogServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 public class LogServiceImpl extends DefaultLogServiceImpl {
     @Override
     public void addLog(OperLogAnnotationEntity logEntity) {
-        log.debug("add  log :{}", logEntity);
+        log.info("add log :{}", logEntity);
         //todo 在这里自己定义访问日志处理的方式
     }
 }
